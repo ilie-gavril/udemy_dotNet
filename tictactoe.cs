8de0c123a66d30ee314215c5ab96585459a2fbf5
ilie-gavril <ilie.gavr@gmail.com>
2023-03-14 20:58:05 +0200

    last work

--- tictactoe.cs
+++ tictactoe.cs
@@ -30,31 +30,31 @@ namespace Udemy_dotNet
             }
             else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
             {
-                WinGame();
+                return WinGame();
             }
             else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
             {
-                WinGame();
+                return WinGame();
             }
             else if ( 
                 !board[0,0].Equals("1") && !board[0, 1].Equals("2") &&
